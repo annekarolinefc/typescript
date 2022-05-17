c2d42e1b509276fb8cce497cd211178260571941
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2022-05-17 15:35:23 -0300

    Atribuindo tipos explícitos

--- 2_tipos/tipos.ts
+++ 2_tipos/tipos.ts
@@ -15,3 +15,20 @@ console.log(idade)
 let possuiHobbies = false
 //possuiHobbies = 1
 console.log(possuiHobbies)
+
+//TIPOS EXPLÍCITOS
+let minhaIdade;  //declarou a variável sem nenhum tipo implicito
+minhaIdade = 27
+console.log(typeof minhaIdade);
+minhaIdade = 'Minha idade é 27'//deste modo, permite alteração
+console.log(typeof minhaIdade)
+
+
+let idadeAtual: any 
+let idadeAntigo: number
+
+
+//atribuindo tipos
+let nomeDoMeio: string = "Fortunato"
+let possuiCNH: boolean = true
+let idade2022: number = 29
