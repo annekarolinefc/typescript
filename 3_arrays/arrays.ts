e7c963183ec035596a7c330cc12d3df1a36a517d
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2022-05-17 15:42:09 -0300

    Arrays

---
+++ 3_arrays/arrays.ts
@@ -0,0 +1,10 @@
+//let hobbies = ["Cozinhar", "Praticar Esportes"]
+let hobbies: any[] = ["Cozinhar", "Praticar Esportes"]
+console.log(hobbies[0])
+console.log(typeof hobbies)
+
+//hobbies = [100]
+
+//após inserir tipo any
+hobbies = [100, 200, 300]
+console.log(hobbies)
