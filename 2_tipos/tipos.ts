4a9f31efba19ffdd343225c8929bde5d64caee0d
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2022-05-17 15:29:26 -0300

    Tipo String, numerico, booleano

---
+++ 2_tipos/tipos.ts
@@ -0,0 +1,17 @@
+//TIPO STRING
+let nome = "João"
+console.log(nome);
+//nome = 28 -> o programa nao permite. Ele entendeu que é do tipo string.
+
+
+//TIPOS NUMERICOS
+let idade = 27
+//idade = 'Ana'
+idade = 27.5 
+console.log(idade)
+
+
+//TIPO BOOLEANO
+let possuiHobbies = false
+//possuiHobbies = 1
+console.log(possuiHobbies)
