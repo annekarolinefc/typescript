0016e9a76771313bfc62084ec6d864a2455b5160
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2022-05-17 16:21:41 -0300

    Enums

---
+++ 5_enums/enums.ts
@@ -0,0 +1,12 @@
+//ESTRUTURA QUE VOCÊ DEFINE VALORES PRÉ-DEFINIDOS
+//EX: DIAS DA SEMANA
+
+enum Cor{
+    Cinza, //0
+    Verde, //1
+    Azul, //2
+    Roxo = 100 //100
+}
+
+let minhaCor: Cor = Cor.Verde
+console.log(minhaCor)
